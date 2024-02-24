c92978e3a9357018bfbb6c463ca3c08223a5be2d
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-02-24 12:28:47 -0600

    adding new app code and making updates to call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+// Source: https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/
+import { get } from 'aws-amplify/api';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([]);
+
+  // Define function to all API
+  async function fetchCoins() {
+    //Get request with latest Amplify
+    const restOperation = await get({
+      apiName: "cryptoapi",
+      path: "/coins"
+    });
+    // Source: https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/#accessing-response-payload
+    const { body } = await restOperation.response;
+    const json = await body.json();
+    updateCoins(json.coins);
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default App;
+export default App
