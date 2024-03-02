2a9323933cacc6f02efda37165d0a17192cd4838
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-03-02 15:38:47 -0600

    added changes for imput for start and limit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,29 @@ const App = () => {
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([]);
 
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
   // Define function to all API
   const fetchCoins = async () => {
+    const { limit, start } = input
+    //Get request with latest Amplify
     const restOperation = await get({
       apiName: "cryptoapi",
-      path: "/coins"
+      path: "/coins",
+      options: { // https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/#accessing-query-parameters--body-in-lambda-proxy-function
+        queryParams: {
+          limit: limit,
+          start: start
+        }
+      }
     });
-
+    // Source: https://docs.amplify.aws/react/build-a-backend/restapi/fetch-data/#accessing-response-payload
     const { body } = await restOperation.response;
     const json = await body.json();
     updateCoins(json.coins);
@@ -27,6 +43,15 @@ const App = () => {
 
   return (
     <div className="App">
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
